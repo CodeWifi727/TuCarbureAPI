e9de9cff5165468b54026b09dede27e1cdd5f41e
Simon42-java <basserue.simon@gmail.com>
2023-07-04 09:32:07 +0200

    feat: :white_check_mark: Get Swagger same Swagger.io
    Swagger
    
    Add Station and rename Weatherfost into carburant

--- Controllers/StationController.cs
+++ Controllers/StationController.cs
@@ -4,24 +4,24 @@ namespace TuCarbureAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class StationController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<StationController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public StationController(ILogger<StationController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetStation")]
+    public IEnumerable<Station> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index => new Station
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
